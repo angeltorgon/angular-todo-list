fd88d9873d66d10b783c0bacea4e70f1e004045e
angel-torres <alas4224@gmail.com>
2019-06-03 17:04:22 -0500

    add button to todo component

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-todo',
+  selector: 'todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+
+  @Input('todo') todo: string;
 
   constructor() { }
 
