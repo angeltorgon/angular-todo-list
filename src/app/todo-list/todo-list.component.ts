94c61abe937673645609302c65053e3cdb9264e9
angel-torres <alas4224@gmail.com>
2019-06-03 16:13:16 -0500

    render list o of todos on screen

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  @Input('todos') todos: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
