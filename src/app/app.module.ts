1a53f3343689265c92e8583d5c9f4b2ad8f23810
angel-torres <alas4224@gmail.com>
2019-06-15 11:32:45 -0500

    removed all components from app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent,
-    TodoComponent
   ],
   imports: [
     BrowserModule,
