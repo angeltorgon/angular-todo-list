e51bb6ce56c7d378766c77bbdb07059c63ad50a7
angel-torres <alas4224@gmail.com>
2019-06-15 12:07:25 -0500

    remade app from scratch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,35 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  todos: any = [
-    {
-      todo: "Learn Angular",
-      id: 1 
-    },
-    
-    {
-      todo: "Learn Node",
-      id: 2 
-    },
-    
-    {
-      todo: "Learn .Net",
-      id: 3 
-    },
-  ];
-  inputText: string = "";
-
-  constructor() {
-    
-  }
-
-  addTodo() {
-    this.todos.push(this.inputText);
-    this.inputText = "";
-  }
-
-  deleteTodo(id) {
-    console.log(id)
-  }
+  title = 'todo-app';
 }
