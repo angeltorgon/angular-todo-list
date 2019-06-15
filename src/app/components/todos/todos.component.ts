611ddfe677d3eb84380e7e902799890b8657d890
angel-torres <alas4224@gmail.com>
2019-06-15 12:46:18 -0500

    generate todos component and use ngFor directive to loop over todos array

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  todos:Todo[];
+
+  constructor() { 
+  }
 
   ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo one',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo two',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Todo three',
+        completed: true
+      }
+    ]
   }
 
 }
